8373fe30d3d9d4926c9389e6062cd8084fe6ab23
tirunahari <maha.tirunahari@gmail.com>
2024-03-30 19:47:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+song = "";
+
+function preload()
+{
+    
+}
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = ceateCapture(VIDEO);
+    video.hide();
+}
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
